0442cf5058804dc7fa722cacda7cd4da5700a6bc
hongdang-sudo <hongdang2465@gmail.com>
2024-12-11 15:12:49 +0700

    Add files via upload

---
+++ Bài 4.1.py
@@ -0,0 +1,10 @@
+print("NGUYEN HONG DANG")
+print("MSSV: 235752021610021")
+print("#############################")
+######################################​
+# Nhập chuỗi S từ bàn phím
+S = input("Nhập chuỗi S: ")
+
+# In ra từng kí tự của S, mỗi kí tự trên một dòng
+for char in S:
+    print(char)
