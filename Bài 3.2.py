c92ada9f72af7a93fe34fead5f699da9a7d441c8
hongdang-sudo <hongdang2465@gmail.com>
2024-12-11 15:08:09 +0700

    Add files via upload

---
+++ Bài 3.2.py
@@ -0,0 +1,8 @@
+print("NGUYEN HONG DANG")
+print("MSSV: 235752021610021")
+print("#############################")
+######################################​
+def sum(a,b):
+    return a+b
+c = sum(4,5);
+print("Tong cua 4 và 5 ="+str(c))
---
+++ Bài 3.6.py
@@ -0,0 +1,12 @@
+print("NGUYEN HONG DANG")
+print("MSSV: 235752021610021")
+print("#############################")
+######################################​
+def get_sum(*num):
+    tmp=0
+    # duyệt các tham số
+    for i in num:
+        tmp +=i
+    return tmp
+result = get_sum(1,2,3,4,5)
+print(result)
