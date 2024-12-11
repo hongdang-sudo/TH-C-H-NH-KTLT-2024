08c5c64ee39fedb67b72795eb4842bcf086fc91a
hongdang-sudo <hongdang2465@gmail.com>
2024-12-11 15:06:36 +0700

    Add files via upload

---
+++ Bài 3.1.py
@@ -0,0 +1,10 @@
+print("NGUYEN HONG DANG")
+print("MSSV: 235752021610021")
+print("#############################")
+######################################​
+def sum(a,b):
+    print("sum="+str(a+b))
+# Tính tổng 2 số 4,5
+sum(4,5)
+# Tính tổng 2 số 3,7
+sum(3,7)
